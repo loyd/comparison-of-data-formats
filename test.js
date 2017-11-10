cc32bf3a3f590fd6720ff27a7aef8a1f0202f4b4
Paul Loyd <pavelko95@gmail.com>
2017-11-11 00:32:27 +0300

    Add colors to the resulting table

--- test.js
+++ test.js
@@ -4,6 +4,7 @@ const assert = require('assert');
 const Benchmark = require('benchmark');
 const prettyBytes = require('pretty-bytes');
 const Table = require('cli-table2');
+const chalk = require('chalk');
 
 const generate = require('./generate');
 
@@ -22,93 +23,108 @@ const compressors = [
     require('./compressors/snappy')
 ];
 
-const data = generate(10);
-const suite = new Benchmark.Suite;
-const info = [];
-const minSize = {title: null, size: Infinity};
+function main(n) {
+    const data = generate(n);
+    const suite = new Benchmark.Suite;
+    const info = [];
 
-for (const encoder of encoders) {
-    const items = [];
+    let minSizeTitle = null;
+    let [minSize, maxSize] = [Infinity, 0];
+    let [minPerf, maxPerf] = [Infinity, 0];
 
-    for (const compressor of compressors) {
-        const title = `${encoder.name} + ${compressor.name}`;
+    for (const encoder of encoders) {
+        const items = [];
 
-        const to = data => compressor.compress(encoder.encode(data));
-        const from = data => encoder.decode(compressor.decompress(data));
+        for (const compressor of compressors) {
+            const title = `${encoder.name} + ${compressor.name}`;
 
-        console.log(`Checking ${title}...`);
+            const to = data => compressor.compress(encoder.encode(data));
+            const from = data => encoder.decode(compressor.decompress(data));
 
-        const encoded = to(data);
-        const actual = clean(from(encoded));
+            console.log(`Checking ${title}...`);
 
-        assert.deepEqual(actual, data);
+            const encoded = to(data);
+            const actual = clean(from(encoded));
 
-        suite.add(title, () => from(to(data)));
+            assert.deepEqual(actual, data);
 
-        items.push({
-            compressor: compressor.name,
-            size: encoded.length
-        });
+            suite.add(title, () => from(to(data)));
 
-        if (encoded.length < minSize.size) {
-            minSize.title = title;
-            minSize.size = encoded.length;
+            const size = encoded.length;
+
+            items.push({
+                compressor: compressor.name,
+                size: size
+            });
+
+            if (size < minSize) {
+                minSizeTitle = title;
+                minSize = size;
+            }
+
+            maxSize = Math.max(size, maxSize);
         }
+
+        info.push({
+            encoder: encoder.name,
+            items,
+        });
     }
 
-    info.push({
-        encoder: encoder.name,
-        items,
-    });
-}
+    console.log('Benchmarking...');
 
-console.log('Benchmarking...');
+    suite
+        .on('cycle', event => {
+            const [encoder, compressor] = event.target.name.split(' + ');
 
-suite
-    .on('cycle', event => {
-        const [encoder, compressor] = event.target.name.split(' + ');
+            const item = info
+                .find(item => item.encoder === encoder)
+                .items
+                .find(item => item.compressor === compressor);
 
-        const item = info
-            .find(item => item.encoder === encoder)
-            .items
-            .find(item => item.compressor === compressor);
+            const perf = item.perf = event.target.hz;
 
-        item.perf = event.target.hz;
+            minPerf = Math.min(perf, minPerf);
+            maxPerf = Math.max(perf, maxPerf);
 
-        console.log(String(event.target), '\t', `${prettyBytes(item.size)}`);
-    })
-    .on('complete', () => {
-        const fastest = suite.filter('fastest').map('name');
-        const smallest = minSize.title;
-
-        console.log(`Fastest is ${fastest}.`);
-        console.log(`Smallest is ${smallest}.`)
-    })
-    .run();
-
-const table = new Table({
-    head: [''].concat(compressors.map(c => ({
-        hAlign: 'center',
-        content: c.name
-    })))
-});
-
-for (const item of info) {
-    table.push({
-        [item.encoder]: item.items.map(i => {
-            const perf = i.perf.toFixed(0) + ' ops/s';
-            const size = prettyBytes(i.size);
-
-            return {
-                hAlign: 'right',
-                content: `${perf}\n${size}`
-            };
+            console.log(String(event.target), '\t', `${prettyBytes(item.size)}`);
         })
-    });
-}
+        .on('complete', () => {
+            const fastest = suite.filter('fastest').map('name');
+            const smallest = minSizeTitle;
 
-console.log();
-console.log(String(table));
+            console.log(`Fastest is ${fastest}.`);
+            console.log(`Smallest is ${smallest}.`)
+        })
+        .run();
+
+    const table = new Table({
+        head: [''].concat(compressors.map(c => ({
+            hAlign: 'center',
+            content: c.name
+        })))
+    });
+
+    for (const item of info) {
+        table.push({
+            [item.encoder]: item.items.map(i => {
+                const perf = i.perf.toFixed(0) + ' ops/s';
+                const size = prettyBytes(i.size);
+
+                const cPerf = colorize(i.perf, minPerf, maxPerf, false)(perf);
+                const cSize = colorize(i.size, minSize, maxSize, true)(size);
+
+                return {
+                    hAlign: 'right',
+                    content: `${cPerf}\n${cSize}`
+                };
+            })
+        });
+    }
+
+    console.log();
+    console.log(String(table));
+}
 
 function clean(obj) {
     for (const name in obj) {
@@ -121,3 +137,18 @@ function clean(obj) {
 
     return obj;
 }
+
+const colors = (
+    'red orange ' +
+    'white '.repeat(5) +
+    'limegreen green'
+).split(' ');
+
+function colorize(val, min, max, invert) {
+    let i = Math.floor((val - min) / (max - min + 1) * colors.length);
+    i = invert ? colors.length - i - 1 : i;
+
+    return chalk.keyword(colors[i]);
+}
+
+main(parseInt(process.argv[2], 10) || 20);
